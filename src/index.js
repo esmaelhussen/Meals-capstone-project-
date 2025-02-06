9727165fd459c9d4380effe3ce13ddd0029bd066
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-06 07:47:14 +0300

    run my several functionalities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import { getMeals, displayMeals } from "./modules/Meals.js";
+import { getTotalMeals } from "./modules/totalMeals.js";
+import { displayTotalMeals } from "./modules/totalMeals.js";
+
+displayMeals();
+displayTotalMeals();
