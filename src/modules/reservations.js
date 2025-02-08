c35a64240855237b8b90b334324e8869e5b43311
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-08 16:21:56 +0300

    add reservation file for creating new reservation and getting the reservation from the involvment API

---
+++ src/modules/reservations.js
@@ -0,0 +1,41 @@
+const involveUrl =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi";
+const appId = "UxI6SiKH6YcJVdhweXUy";
+
+export const getReservations = async (itemId) => {
+  try {
+    const response = await fetch(
+      `${involveUrl}/apps/${appId}/reservations?item_id=${itemId}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch comments: ${response.status}`);
+    }
+
+    const text = await response.text();
+    if (!text) {
+      return [];
+    }
+
+    const data = JSON.parse(text);
+    return Array.isArray(data) ? data : [];
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    return [];
+  }
+};
+
+export const postReservation = async (itemId, userName, dateStart, dateEnd) => {
+  await fetch(`${involveUrl}/apps/${appId}/reservations`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      item_id: itemId,
+      username: userName,
+      date_start: dateStart,
+      date_end: dateEnd,
+    }),
+  });
+};
