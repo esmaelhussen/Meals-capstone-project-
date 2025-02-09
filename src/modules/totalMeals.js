1b3a5e7816774c426cbad2c83aa49c4c3e57206e
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-09 14:21:47 +0300

    instead of getting the number of total comments and reservations getting them from function by calculating using forEach

--- src/modules/totalMeals.js
+++ src/modules/totalMeals.js
@@ -1,4 +1,6 @@
 import { getMeals } from "./getMeals.js";
+import { getComments, postComment } from "./comments.js";
+import { getReservations } from "./reservations.js";
 
 //instead of getting numbers from the API calculating it with foreach loop
 
@@ -21,4 +23,24 @@ export const displayTotalMeals = async () => {
   const meals = document.querySelector(".js-total-meal");
   return (meals.innerHTML = `
   <h2 class="total-meal">Meals(${totalMeals})</h2>`);
+};
+
+export const getTotalComments = async (itemId) => {
+  const comments = await getComments(itemId);
+  let totalComments = 0;
+
+  comments.forEach(() => {
+    totalComments++;
+  });
+  return totalComments;
+};
+
+export const getTotalReservations = async (itemId) => {
+  const reservations = await getReservations(itemId);
+  let totalReservations = 0;
+
+  reservations.forEach(() => {
+    totalReservations++;
+  });
+  return totalReservations;
 };
