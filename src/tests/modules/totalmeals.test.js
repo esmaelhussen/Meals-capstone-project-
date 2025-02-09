1960be238de49656a84f5ddcc912ba0b63d9e83f
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-09 12:25:08 +0300

    add some test files

---
+++ src/tests/modules/totalmeals.test.js
@@ -0,0 +1,25 @@
+import { getTotalMeals } from "../../modules/totalMeals.js";
+import { getMeals } from "../../modules/getMeals.js";
+
+jest.mock("../../modules/getMeals.js", () => ({
+  getMeals: jest.fn(),
+}));
+
+describe("getTotalMeals", () => {
+  it("should return the total number of meals", async () => {
+    const mockMeals = [{}, {}, {}, {}, {}];
+    getMeals.mockResolvedValue(mockMeals);
+
+    const result = await getTotalMeals();
+
+    expect(result).toBe(5);
+  });
+
+  it("should return 0 if there are no meals", async () => {
+    getMeals.mockResolvedValue([]);
+
+    const result = await getTotalMeals();
+
+    expect(result).toBe(0);
+  });
+});
